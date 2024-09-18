ac2f56de8774fb402e7f323b2c3498640ecd8d99
eliasgo <you@example.com>
2024-09-18 17:36:35 +0200

    Changed the position of the Git repo and made a MVP on CI/CD

--- CID/astro.config.mjs
+++ CID/astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import vercel from '@astrojs/vercel/serverless';
+import vercel from "@astrojs/vercel/serverless";
 
-import starlight from '@astrojs/starlight';
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: "server",
 
   adapter: vercel({
     webAnalytics: { enabled: true },
@@ -14,41 +14,44 @@ export default defineConfig({
 
   integrations: [
     starlight({
-      title: 'Velkommen til Git',
+      title: "Velkommen til Git",
 
       logo: {
-        light: './src/assets/Git-Icon-Black.svg',
-        dark: './src/assets/Git-Icon-White.svg',
+        light: "./src/assets/Git-Icon-Black.svg",
+        dark: "./src/assets/Git-Icon-White.svg",
         replacesTitle: false,
       },
 
       sidebar: [
-        { label: 'Velkommen!', link: '/' },
-        { label: 'Git-stages', autogenerate: { directory: '/git-levels/' } },
-        {
-          label: 'Git-kommandoer',
-          autogenerate: { directory: '/git-commands/' },
-        },
+        { label: "Velkommen!", link: "/" },
+
+        { label: "Git", autogenerate: { directory: "/git/" } },
+        // { label: "Git-stages", autogenerate: { directory: "/git-levels/" } },
+        // {
+        //   label: "Git-kommandoer",
+        //   autogenerate: { directory: "/git-commands/" },
+        // },
+        { label: "CI/CD", autogenerate: { directory: "/CICD/" } },
       ],
 
       tableOfContents: false,
 
       locales: {
         root: {
-          label: 'Norsk',
-          lang: 'nb',
+          label: "Norsk",
+          lang: "nb",
         },
       },
 
       components: {
         // Override the default `Head` component to add tracking.
-        SocialIcons: './src/components/TrackingHead.astro',
+        SocialIcons: "./src/components/TrackingHead.astro",
 
         // Override the default `Footer` component to add cookie-info.
-        Footer: './src/components/CookieFooter.astro',
+        Footer: "./src/components/CookieFooter.astro",
 
         // Override the default `Sidebar` component to add a custom sidebar with feedback form.
-        Sidebar: './src/components/FeedbackNavbar.astro',
+        Sidebar: "./src/components/FeedbackNavbar.astro",
       },
     }),
   ],
